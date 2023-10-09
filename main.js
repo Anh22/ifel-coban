f5f484ea13b9dd588b0115598201c0d8fb127925
unknown <phamquocanh072001@gmail.com>
2023-10-09 14:32:04 +0700

    b

---
+++ main.js
@@ -0,0 +1,102 @@
+
+
+// Bài 1: Nhập vào hai số a và b, và kiểm tra xem a có chia hết cho b hay không
+//// bài 1:
+// // let a = prompt("a");
+// // let b=prompt("b");
+// // if(a%b==0) {
+// //     alert('a chia hết cho b')
+// // }
+// // else{
+// //     alert('a không chia hết cho b')
+// // }
+// Bài 2: Nhập tuổi và in ra kết quả nếu tuổi học sinh đó không đủ điều kiện vào học lớp 10.
+//// bài 2
+// let a = prompt('Nhập tuổi');
+// if(a<16){
+//     alert('hs không đủ tuổi học lớp 10')
+// }
+// else {
+//     alert('hs  đủ tuổi học lớp 10')
+// }
+// Bài 3: Nhập một số nguyên bất kỳ và in kết quả ra màn hình để nói cho người dùng biết số đó là lớn hay nhỏ hơn 0
+// let a = prompt('Nhập vào một sô:')
+// if(a<0){
+//     alert('la số nguyên âm')
+// }
+// else {
+//     alert('là số nguyên dương')
+// }
+//
+// //
+// Bài 4: Nhập 3 số nguyên và tìm giá trị lớn nhất của ba số nguyên đó
+// let a = prompt("Nhập a")
+// let b = prompt('Nhập b')
+// let c = prompt('Nhập c')
+// max= a;
+// if(max<b){
+//     max=b;
+//     alert('giá trị lớn nhất la'+b)
+// }
+// else if(max<c){
+//     max=c
+//     alert('giá trị lớn nhất là'+c)
+// }
+// else{
+//     alert('sô lớn nhất là '+a)
+// }
+
+//
+// Bài 5: Xếp hạng học lực của học sinh dựa trên các điểm bài kiểm tra, điểm thi giữa kỳ, điểm thi cuối kỳ
+// let a = prompt("Nhập vào điểm kiểm tra")
+// let b=prompt('Nhập vào điểm thi')
+// let c = prompt('Nhập vào điểm cuối kỳ')
+// let  d = (a+b+c)/3;
+// if (d<5){
+//     alert('Học lực trung bình'+d)
+// }
+// else if (5<d<7.5)
+// {
+//     alert('Học lực Khá')
+// }
+// else if (d>7.5){
+//     alert('Học lực giỏi')
+//     }
+//
+
+// Bài 6: Tính hoa hồng nhận được tuỳ theo mức doanh số bán hàng
+// var sales = 10000; // Ví dụ: doanh số bán hàng là 10,000 đơn vị tiền tệ
+// var commissionRate = 5; // Ví dụ: tỷ lệ hoa hồng là 5%
+//
+// // Tính hoa hồng
+// var commission = (sales * commissionRate) / 100;
+//
+// // In ra kết quả
+// console.log("Hoa hồng nhận được là: " + commission + " đơn vị tiền tệ");
+
+//
+// Bài 7: Tính cước điện thoại cho một hộ gia đình với các thông số đã cho
+// Các thông số
+// var giaCuocCoBan = 20; // Giá cước cơ bản hàng tháng
+// var soPhutGoi = 300; // Số phút gọi điện thoại trong tháng
+// var giaCuocMoiPhut = 0.1; // Giá cước mỗi phút gọi
+//
+// // Các chi phí bổ sung
+// var phiSMS = 5; // Chi phí tin nhắn SMS
+// var phiData = 15; // Chi phí dữ liệu di động
+//
+// // Tính cước điện thoại hàng tháng
+// var tongCuocDienThoai = giaCuocCoBan + (soPhutGoi * giaCuocMoiPhut) + phiSMS + phiData;
+//
+// // In ra kết quả
+// console.log("Tổng cước điện thoại hàng tháng là: " + tongCuocDienThoai + " đơn vị tiền tệ");
+
+// Để hoàn thành bài thực hành, học viên cần:
+//
+//     Đưa mã nguồn lên GitHub
+// Dán link của repository lên phần nộp bài trên CodeGymX
+// bài 3
+
+
+
+
